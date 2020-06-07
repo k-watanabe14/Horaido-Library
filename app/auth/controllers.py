72c9b2c6e52ce6cd5ee544856dde850ccfc14922
k-watanabe-macbook-air <kazuma.watanabe1@ibm.com@kw-MacBook-Air.local>
2020-06-07 09:51:59 +0900

    improve the function of sign up

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -2,6 +2,7 @@ import functools
 from flask import Blueprint, request, render_template, flash, session, redirect, url_for, g
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.auth.models import User
+from app import db
  
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -12,25 +13,21 @@ def signup():
 
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif db.session.query(User).filter(User.username == username).count() != 0:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            data = User(username, email, generate_password_hash(password))
+            db.session.add(data)
+            db.session.commit()
             return redirect(url_for('auth.login'))
 
         flash(error)
@@ -43,9 +40,10 @@ def login():
 
     if request.method == 'POST':
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
-        db = get_db()
         error = None
+
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
--- app/auth/models.py
+++ app/auth/models.py
@@ -6,13 +6,13 @@ class User(db.Model):
     __tablename__ = 'auth_user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(192), nullable=False)
 
-    def __init__(self, name, email, password):
+    def __init__(self, username, email, password):
 
-        self.name = name
+        self.username = username
         self.email = email
         self.password = password
 
