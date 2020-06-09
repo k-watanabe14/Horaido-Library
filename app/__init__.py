7fffd6af09435aa272d470c1ae39e15a87b7f798
k-watanabe-macbook-air <kazuma.watanabe1@ibm.com@kw-MacBook-Air.local>
2020-06-09 12:44:41 +0900

    make function to register a book

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- app/register/models.py
+++ app/register/models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy import BigInteger
 
 
 class Book(db.Model):
@@ -6,23 +7,23 @@ class Book(db.Model):
     __tablename__ = 'book'
 
     id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.Integer, nullable=False)
+    isbn = db.Column(db.BigInteger, nullable=False)
     title = db.Column(db.String(128), nullable=False)
     author = db.Column(db.String(128))
     publisher_name = db.Column(db.String(128))
     sales_date = db.Column(db.String(128))
-    medium_image_url = db.Column(db.String(128))
+    image_url = db.Column(db.String(128))
     donor = db.Column(db.String(128))
 
 
-    def __init__(self, isbn, title, author, publisher_name, sales_date, medium_image_url, donor):
+    def __init__(self, isbn, title, author, publisher_name, sales_date, image_url, donor):
 
         self.isbn = isbn
         self.title = title
         self.author = author
         self.publisher_name = publisher_name
         self.sales_date = sales_date
-        self.medium_image_url = medium_image_url
+        self.image_url = image_url
         self.donor = donor
 
     def __repr__(self):
