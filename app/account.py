6d3b965def5b1348adeca3e8b9960183a76843f4
k-watanabe14 <east-andante@hotmail.co.jp>
2020-12-20 09:31:56 +0900

    Make login/signup forms more generic

--- app/account.py
+++ app/account.py
@@ -17,14 +17,14 @@ def index():
     return render_template('account/index.html')
 
 
-@mod_account.route('/password_reset/', methods=('GET', 'POST'))
-def password_reset():
+@mod_account.route('/request_reset_password/', methods=('GET', 'POST'))
+def request_reset_password():
 
     form = RequestResetForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()  
-        send_reset_email(user)  
+        send_email_for_reset(user)  
         flash('パスワード再設定のメールを送りました。')  
         return redirect(url_for('auth.login'))
 
@@ -33,17 +33,17 @@ def password_reset():
             for error in errors:
                 flash(error)
 
-    return render_template('account/password_reset.html', form=form)  
+    return render_template('account/request_reset_password.html', form=form)  
   
   
-@app.route('/new_password/<token>', methods=['GET', 'POST'])  
-def new_password(token):  
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])  
+def reset_password(token):  
 
     user = User.verify_reset_token(token)  
 
     if user is None:  
         flash('無効のURLです。', 'warning')  
-        return redirect(url_for('account.password_reset'))  
+        return redirect(url_for('account.request_reset_password'))  
 
     form = ResetPasswordForm()  
 
@@ -60,15 +60,15 @@ def new_password(token):
             for error in errors:
                 flash(error)
     
-    return render_template('account/new_password.html', form=form)
+    return render_template('account/reset_password.html', form=form)
 
 
-def send_reset_email(user):  
+def send_email_for_reset(user):  
     
     token = user.get_reset_token()  
     
     msg = Message('パスワード再設定', sender="朋来堂ライブラリー", recipients=[user.email])  
     
-    msg.body = '''パスワードの再設定がリクエストされました。リンク先からパスワードを再設定してください。\r\n {url} \r\n \r\n パスワードを変更する必要がない場合は、何もする必要はありません。'''.format(url=url_for('new_password', token=token, _external=True))  
+    msg.body = '''パスワードの再設定がリクエストされました。リンク先からパスワードを再設定してください。\r\n {url} \r\n \r\n パスワードを変更する必要がない場合は、何もする必要はありません。'''.format(url=url_for('reset_password', token=token, _external=True))  
     
     mail.send(msg)
