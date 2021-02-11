3162b13f52e051b11021ead280f2f810e1d5f319
k-watanabe14 <east-andante@hotmail.co.jp>
2021-02-11 16:31:38 +0900

    Fix data sql

--- tests/data.sql
+++ tests/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO auth_user (username, email, password)
+INSERT INTO auth_user (username, email, password, admin)
 VALUES
-  ('Jone', 'jone@gmail.com', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-  ('わたなべ', 'watanabe@outlook.co.jp', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79'),
-  ('山田太郎', 'taro.yamada@yahoo.co.jp', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79');
+  ('Jone', 'jone@gmail.com', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', true),
+  ('わたなべ', 'watanabe@outlook.co.jp', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', false),
+  ('山田太郎', 'taro.yamada@yahoo.co.jp', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', false);
 
 INSERT INTO book (isbn, title, author, publisher_name, sales_date, image_url, borrower_id, checkout_date)
 VALUES
